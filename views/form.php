227fae62f0770ff6e19f0bd84ab4a6eb43b73b5a
Pavel Anderle <anderle@vozik1-MacBook-Pro.local>
2019-02-05 20:47:33 +0100

    completation form.php

--- views/form.php
+++ views/form.php
@@ -1,5 +1,16 @@
+<h1>Směnárna 1.O</h1>
 <form class="" action="index.php" method="post">
-<label for="many"></label>
-  <input type="text" name="many" >
+  <label for="many"></label>
+  <input type="text" name="money" >
+  <label for="">Měna</label>
+  <select class="" name="Currency">
+    <option value="KC">Česká koruna</option>
+    <option value="EUR">Euro</option>
+    <option value="USD">US dolar</option>
+  </select>
+  <label for="typeChange">Nákup</label>
+  <input type="radio" name="typeChange" value="purchase">
+  <label for="typeChange">Prodej</label>
+  <input type="radio" name="typeChange" value="sale">
   <input type="submit" name="submit" value="Převeď">
 </form>
