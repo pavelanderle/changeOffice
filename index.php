7999bf9cf207bf929d1ff7d5cf7a098b799bd48c
Pavel Anderle <anderle@vozik1-MacBook-Pro.local>
2019-02-05 19:46:43 +0100

    create structure MVC

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-  
+  require_once("model/function.php");
  ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Change Office 1.0</title>
+  </head>
+  <body>
+    <?php
+       require_once("views/form.html");
+     ?>
+  </body>
+</html>
